805dc588e22dabb2b1b68005f00f6fab669877b6
Jonathan Delanoye <delano55@imec.be>
2022-09-02 11:03:11 +0200

    fix(plwd info): make sure to account for null values

--- web/src/hooks/usePlwdValidationSchema.ts
+++ web/src/hooks/usePlwdValidationSchema.ts
@@ -20,7 +20,7 @@ export const usePlwdValidationSchema = () => {
     watchId: yup
       .string()
       .test('len', 'Length must be exactly 15 digits', (value = '') => {
-        if (value.length === 0) return true;
+        if (!value || value.length === 0) return true;
 
         return value.length === 15;
       })
